5cec24e940852e98ef45a34cd3b1c745795b3d42
Daniel <44358467+danon321@users.noreply.github.com>
2024-04-20 00:54:10 +0200

    Add loading UI, fetch data separately

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import { ReactNode } from 'react';
+
+export default async function Page() {
+  const data: ReactNode = await new Promise((resolve) =>
+    setTimeout(() => resolve('Invoices Page'), 2000),
+  );
+  return <p>{data}</p>;
 }
