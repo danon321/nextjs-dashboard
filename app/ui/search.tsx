1e8176ace126ab0b8003fafbbd72781f74b6c42d
Daniel <44358467+danon321@users.noreply.github.com>
2024-04-21 23:33:21 +0200

    Set search info in URL

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,35 @@
 'use client';
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
+import { ChangeEvent } from 'react';
 
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(e: ChangeEvent<HTMLInputElement>) {
+    const params = new URLSearchParams(searchParams);
+    const { value } = e.target;
+
+    if (value) {
+      params.set('query', value);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
         Search
       </label>
       <input
+        onChange={handleSearch}
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
